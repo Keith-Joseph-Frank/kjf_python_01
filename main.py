b9c161497f1fdcc4d87b910b14da1b2c99053dd9
Keith-Joseph-Frank <78824656+Keith-Joseph-Frank@users.noreply.github.com>
2022-02-07 02:23:38 -0500

    Update main.py
    Added colorama text colorization code.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello Keith Frank, how are you?')
+# Python program to print
+# red text with green background
+
+from colorama import Fore, Back, Style
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
+
